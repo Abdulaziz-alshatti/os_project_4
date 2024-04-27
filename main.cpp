f753add81008b59b4dc5b23824fe6199dec2cad4
Abdulaziz-alshatti <114772697+Abdulaziz-alshatti@users.noreply.github.com>
2024-04-27 13:11:06 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <thread>
 #include "MemoryManager.h"
 #include "Process.h"
+#include <mutex>
+using namespace std;
 
 int main(int argc, char** argv) {
     if (argc < 3) {
@@ -14,10 +16,15 @@ int main(int argc, char** argv) {
 
     std::string inputFile = argv[1];
     std::string outputFile = argv[2];
-    int seed = (argc > 3) ? std::stoi(argv[3]) : -1;
+    std::ofstream outFile(outputFile);
+    if (!outFile) {
+        std::cerr << "Error opening output file: " << outputFile << std::endl;
+        return 1;
+    }
 
     std::ifstream file(inputFile);
     if (!file) {
+        outFile << "Error opening input file: " << inputFile << std::endl;
         std::cerr << "Error opening input file: " << inputFile << std::endl;
         return 1;
     }
@@ -25,8 +32,8 @@ int main(int argc, char** argv) {
     size_t mainMemorySize, pageSize;
     int processCount;
     file >> mainMemorySize >> pageSize >> processCount;
-
     if (!file) {
+        outFile << "Error reading configuration from input file" << std::endl;
         std::cerr << "Error reading configuration from input file" << std::endl;
         return 1;
     }
@@ -34,6 +41,7 @@ int main(int argc, char** argv) {
     std::vector<std::string> threadFiles(processCount);
     for (int i = 0; i < processCount; ++i) {
         if (!(file >> threadFiles[i])) {
+            outFile << "Error reading thread file name for process " << i << std::endl;
             std::cerr << "Error reading thread file name for process " << i << std::endl;
             return 1;
         }
@@ -41,27 +49,31 @@ int main(int argc, char** argv) {
 
     file.close();
 
-    MemoryManager memoryManager(mainMemorySize, pageSize);
-
-    std::cout << "Main Memory Size: " << mainMemorySize << " bytes" << std::endl;
-    std::cout << "Page Size: " << pageSize << " bytes" << std::endl;
-    std::cout << "Number of Processes: " << processCount << std::endl;
+    MemoryManager memoryManager(mainMemorySize, pageSize, outFile);
+    cout << "Main Memory Size: " << mainMemorySize << " bytes\n"
+            << "Page Size: " << pageSize << " bytes\n"
+            << "Number of Processes: " << processCount << "\n";
     for (const auto& fileName : threadFiles) {
-        std::cout << "Thread File: " << fileName << std::endl;
+        cout << "Thread File: " << fileName << "\n";
     }
-
+     
+   std::mutex outFileMutex;
     std::vector<std::thread> threads;
     for (int i = 0; i < processCount; i++) {
-        threads.emplace_back([&memoryManager, &threadFiles, i]() {
-            Process process(i, threadFiles[i], memoryManager);
-            process.run();
-        });
-    }
+    threads.emplace_back([&memoryManager, &threadFiles, &outFile, &outFileMutex, i]() {
+        Process process(i, threadFiles[i], memoryManager, outFile, outFileMutex);
+        process.run();
+    });
+}
+
+
 
     for (auto& thread : threads) {
         thread.join();
     }
 
+    outFile << "Simulation completed.\n";
     std::cout << "Simulation completed." << std::endl;
+    outFile.close();
     return 0;
 }
