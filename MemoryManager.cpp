e5d5ab8cf26d318efe11628d2ee61c4165915754
Abdulaziz-alshatti <114772697+Abdulaziz-alshatti@users.noreply.github.com>
2024-04-27 13:11:41 +0500

    Update MemoryManager.cpp

--- MemoryManager.cpp
+++ MemoryManager.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
-MemoryManager::MemoryManager(size_t memorySize, size_t pageSize)
-    : totalMemorySize(memorySize), pageSize(pageSize), mainMemory(memorySize, 0), freeFrames(memorySize / pageSize, true) {
+#include <fstream>
+MemoryManager::MemoryManager(size_t memorySize, size_t pageSize, std::ofstream& outFile)
+: totalMemorySize(memorySize), pageSize(pageSize), mainMemory(memorySize, 0), freeFrames(memorySize / pageSize, true), outputStream(outFile) {
     initializeMemory();
 }
 
@@ -21,15 +21,15 @@ void MemoryManager::initializeFreeFrames() {
 
 char MemoryManager::readMemory(size_t physicalAddress) {
     if (physicalAddress >= totalMemorySize) {
-        std::cerr << "Error: Physical address out of bounds." << std::endl;
-        return -1;
+        outputStream << "Error: Physical address out of bounds." << std::endl;
+        return -1;  // Make sure to handle error appropriately
     }
     return mainMemory[physicalAddress];
 }
 
 void MemoryManager::writeMemory(size_t physicalAddress, char value) {
     if (physicalAddress >= totalMemorySize) {
-        std::cerr << "Error: Physical address out of bounds." << std::endl;
+        outputStream << "Error: Physical address out of bounds." << std::endl;
         return;
     }
     mainMemory[physicalAddress] = value;
@@ -39,6 +39,7 @@ int MemoryManager::allocateFrame() {
     for (size_t i = 0; i < freeFrames.size(); i++) {
         if (freeFrames[i]) {
             freeFrames[i] = false;
+             
             return i;
         }
     }
@@ -49,19 +50,18 @@ void MemoryManager::freeFrame(int frameIndex) {
     if (frameIndex >= 0 && frameIndex < static_cast<int>(freeFrames.size())) {
         freeFrames[frameIndex] = true;
     } else {
-        std::cerr << "Error: Invalid frame index." << std::endl;
+        outputStream << "Error: Invalid frame index." << std::endl;
     }
 }
 
 void MemoryManager::initializeFrame(int frameIndex) {
-    std::srand(static_cast<unsigned int>(std::time(nullptr) + frameIndex)); // Seed based on time and frame index
+    std::srand (time(NULL));
     int start = frameIndex * pageSize;
     for (int i = 0; i < pageSize; i++) {
-        mainMemory[start + i] = std::rand() % 256;
-        
+        mainMemory[start + i] = std::rand() ;  // Randomly initialize the frame
     }
+     
 }
-
 
 size_t MemoryManager::getFreeFrameCount() const {
     return std::count(freeFrames.begin(), freeFrames.end(), true);
